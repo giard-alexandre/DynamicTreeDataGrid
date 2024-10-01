13223a7ba6dcd505a023376251a800afe3babee7
Alexandre Giard <giard.alexandre@gmail.com>
2024-09-30 19:55:02 -0500

    fix: catch errors when improper drop

--- src/DynamicTreeDataGrid/Controls/ColumnListView.axaml.cs
+++ src/DynamicTreeDataGrid/Controls/ColumnListView.axaml.cs
@@ -25,58 +25,26 @@ public partial class ColumnListView : UserControl {
     }
 
     private async void OnPointerPressed(object? sender, PointerPressedEventArgs e) {
-        Console.WriteLine("DoDrag start");
-
+        // We don't care about anything that isn't one of our list items.
+        // TODO: Change to a custom control?
         if (sender is not Border border) return;
         if (border.DataContext is not IColumn column) return;
 
         _draggedItem = border;
 
-        // var ghostPos = GhostItem.Bounds.Position;
-        // _ghostPosition = new Point(ghostPos.X + _mouseOffset.X, ghostPos.Y + _mouseOffset.Y);
-
-        // var mousePos = e.GetPosition(MainContainer);
-        // var offsetX = mousePos.X - ghostPos.X;
-        // var offsetY = mousePos.Y - ghostPos.Y + _mouseOffset.X;
-        // GhostItem.RenderTransform = new TranslateTransform(offsetX, offsetY);
-
-        // if (DataContext is not DragAndDropPageViewModel vm) return;
-        // vm.StartDrag(column);
-
-        // GhostItem.IsVisible = true;
-
         var dragData = new DataObject();
         dragData.Set(DragItemFormat, column);
-        var result = await DragDrop.DoDragDrop(e, dragData, DragDropEffects.Move);
-        var finalPosition = e.GetPosition(MainContainer);
-        Console.WriteLine($"DragAndDrop result: {result}");
-
-        // GhostItem.IsVisible = false;
+        await DragDrop.DoDragDrop(e, dragData, DragDropEffects.Move);
     }
 
     private void DragOver(object? sender, DragEventArgs e) {
-        Console.WriteLine("DragOver");
-        Console.WriteLine(sender);
-        Console.WriteLine(e.DragEffects);
-
-        //
-        // GhostItem.RenderTransform = new TranslateTransform(offsetX, offsetY);
-
         // set drag cursor icon
         e.DragEffects = DragDropEffects.Move;
 
-        // if (DataContext is not DragAndDropPageViewModel vm) return;
-        var data = e.Data.Get(DragItemFormat);
-        if (data is not IColumn column) return;
-
-        // if (!vm.IsDestinationValid(taskItem, (e.Source as Control)?.Name))
-        // {
-        //     e.DragEffects = DragDropEffects.None;
-        // }
-
-        var currentPosition = e.GetPosition(MainContainer);
-
         if (_draggedItem is null) return;
+
+        // Render the item under the mouse.
+        var currentPosition = e.GetPosition(MainContainer);
         var offsetX = currentPosition.X - _draggedItem.Bounds.Position.X;
         var offsetY = currentPosition.Y - _draggedItem.Bounds.Position.Y;
         _draggedItem.RenderTransform = new TranslateTransform(offsetX, offsetY);
@@ -84,8 +52,6 @@ public partial class ColumnListView : UserControl {
     }
 
     private void Drop(object? sender, DragEventArgs e) {
-        Console.WriteLine("Drop");
-
         var data = e.Data.Get(DragItemFormat);
 
         if (data is not IDynamicColumn sourceColumn) {
@@ -93,18 +59,20 @@ public partial class ColumnListView : UserControl {
             return;
         }
 
+        // Remove dragged item transform if its set.
+        if (_draggedItem is not null) {
+            _draggedItem.RenderTransform = null;
+        }
+
+        // Get the list element where we dropped the source element. (e.Source is the "target" in this case)
         if (e.Source is not Control control || !TryGetColumn(control, out var targetColumn)) {
             Console.WriteLine("Invalid Drop Target");
             return;
         }
 
+        // Move the item in the collection
         if (DataContext is IDynamicColumns collection) {
             collection.Move(sourceColumn, targetColumn);
-        }
-
-        // Remove dragged item transform if its set.
-        if (_draggedItem is not null) {
-            _draggedItem.RenderTransform = null;
         }
     }
 

5f8fe432b2406c69f8dcd6547c1ffb94c2784cbb
Alexandre Giard <giard.alexandre@gmail.com>
2024-10-13 17:00:25 -0500

    feat: add datasource to colstate test

--- tests/DynamicTreeDataGrid.Tests/DynamicFlatTreeDataGridSource/ColumnStates.cs
+++ tests/DynamicTreeDataGrid.Tests/DynamicFlatTreeDataGridSource/ColumnStates.cs
@@ -1,21 +1,58 @@
-using TUnit.Core;
+using System.Collections.ObjectModel;
+
+using Bogus;
+
+using DynamicData.Binding;
+
+using DynamicTreeDataGrid.Columns;
+
+using Microsoft.Reactive.Testing;
+
+using ReactiveUI.Testing;
 
 namespace DynamicTreeDataGrid.Tests.DynamicFlatTreeDataGridSource;
 
 public class ColumnStates {
+    private DynamicFlatTreeDataGridSource<TestPerson, int> _source;
 
-	[Before(Test)]
-	public async Task Setup()
-	{
-		await Task.CompletedTask;
-	}
+    private readonly TestScheduler _scheduler = new();
 
-	[Test]
-	public async Task MyTest() {
-		var result = true;
+    [Before(Test)]
+    public async Task Setup() {
+        //Set the randomizer seed to generate repeatable data sets.
+        Randomizer.Seed = new Random(8675309);
+        var fakeData = new Faker<TestPerson>()
+            .CustomInstantiator(f => new TestPerson(f.IndexFaker, f.Name.FullName(), f.Person.DateOfBirth))
+            .Generate(300);
+        var data = new ObservableCollection<TestPerson>(fakeData).ToObservableChangeSet(person => person.Id);
 
-		await Assert.That(result).IsEqualTo(true);
-	}
+
+        _source = new DynamicFlatTreeDataGridSource<TestPerson, int>(data, _scheduler) {
+            Columns = {
+                new DynamicTextColumn<TestPerson,int>("Id", "Id", person => person.Id),
+                new DynamicTextColumn<TestPerson,string>("Name", "Name", person => person.Name),
+                new DynamicTextColumn<TestPerson,DateTime>("Date-of-Birth", "DoB", person => person.DateOfBirth),
+            },
+        };
+        await Task.CompletedTask;
+    }
+
+    [After(Test)]
+    public async Task CleanupAfterTests() {
+        await Task.CompletedTask;
+        _source.Dispose();
+    }
+
+    [Test]
+    public async Task MyTest() {
+        await _scheduler.WithAsync(async testScheduler => {
+            // Tick once to get ChangeSet data
+            testScheduler.AdvanceBy(1);
+            await Assert.That(_source.Columns.Count).IsEqualTo(3);
+
+        });
+
+    }
 }
 
-internal record TestPerson(uint Id, string Name, uint Age);
+internal record TestPerson(int Id = 0, string Name = "", DateTime DateOfBirth = new());

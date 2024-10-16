eb81c62b7852b4d5603c0145c2fb68db9bd60c9c
Alexandre Giard <giard.alexandre@gmail.com>
2024-10-16 14:27:54 -0500

    feat: add gridsource options

---
+++ src/DynamicTreeDataGrid/DynamicTreeDataGridSourceOptions.cs
@@ -0,0 +1,27 @@
+﻿using DynamicData;
+
+using DynamicTreeDataGrid.Models.Sorting;
+
+namespace DynamicTreeDataGrid;
+
+public class DynamicTreeDataGridSourceOptions<TModel> {
+
+    /// <summary>
+    /// Sort to apply BEFORE the requested column sort.
+    /// Think of this as: <c>Sort(PreColumnSort.ThenBy(ColumnSort).ThenBy(PostColumnSort)</c>
+    /// </summary>
+    public IComparer<TModel> PreColumnSort { get; set; } = new NoSortComparer<TModel>();
+
+    /// <summary>
+    /// Sort to apply AFTER the requested column sort.
+    /// Think of this as: <c>Sort(PreColumnSort.ThenBy(ColumnSort).ThenBy(PostColumnSort)</c>
+    /// </summary>
+    public IComparer<TModel> PostColumnSort { get; set; } = new NoSortComparer<TModel>();
+
+    /// <summary>
+    /// Sort optimization setting to apply to ALL sorts.
+    /// Includes the pre-columns and post-column sorts as well as the column sort itself.
+    /// You must be VERY sure that all sort possibilities can adhere to the <see cref="SortOptimisations"/> selected.
+    /// </summary>
+    public SortOptimisations SortOptimisations { get; init; } = SortOptimisations.None;
+}

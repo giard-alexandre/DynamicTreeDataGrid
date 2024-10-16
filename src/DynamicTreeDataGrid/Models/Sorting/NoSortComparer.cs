738393e004b2e841f840de676e192404b5d3d11f
Alexandre Giard <giard.alexandre@gmail.com>
2024-10-15 21:08:51 -0500

    chore: formatting

--- src/DynamicTreeDataGrid/Models/Sorting/NoSortComparer.cs
+++ src/DynamicTreeDataGrid/Models/Sorting/NoSortComparer.cs
@@ -1,9 +1,9 @@
 ﻿namespace DynamicTreeDataGrid.Models.Sorting;
 
 /// <summary>
-///     A comparer that doesn't affect the comparison at all (will not change the order of items)
+/// A comparer that doesn't affect the comparison at all (will not change the order of items)
 /// </summary>
 /// <typeparam name="T"></typeparam>
 public class NoSortComparer<T> : IComparer<T> {
-    public int Compare(T? x, T? y) => 0;
+	public int Compare(T? x, T? y) => 0;
 }
